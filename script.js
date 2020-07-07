a43fb2dc4a86035807a8b72db13641965b608410
Tomas Ramirez <ramirez.tomas@yahoo.com>
2020-07-06 19:57:19 -0700

    Refactor Code

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const password2 = document.getElementById('password2');
 
 
 // Show Input Error Message
-function showError(input,message){
+function showError(input, message) {
     const formControl = input.parentElement;
     formControl.className = 'form-control error';
     const small = formControl.querySelector('small');
@@ -21,20 +21,20 @@ function showSuccess(input) {
 }
 
 // Check Email Is Valid
-function checkEmail(input){
+function checkEmail(input) {
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     if (re.test(input.value.trim())) {
         showSuccess(input);
-     } else {
+    } else {
         showError(input, 'Email is not valid')
     }
 }
 
 
 //Check Required Fields
-function checkRequired(inputArr){
-    inputArr.forEach(function(input){
-        if(input.value.trim() === ''){
+function checkRequired(inputArr) {
+    inputArr.forEach(function (input) {
+        if (input.value.trim() === '') {
             showError(input, `${getFieldName(input)} is required`);
         } else {
             showSuccess(input);
@@ -44,10 +44,10 @@ function checkRequired(inputArr){
 
 
 // Check Input Length
-function checkLength(input, min, max){
-    if(input.value.length < min) {
+function checkLength(input, min, max) {
+    if (input.value.length < min) {
         showError(input, `${getFieldName(input)} must be at least ${min} characters`);
-    } else if (input.value.length > max){
+    } else if (input.value.length > max) {
         showError(input, `${getFieldName(input)} must be at less ${max} characters`);
     } else {
         showSuccess(input);
@@ -56,27 +56,27 @@ function checkLength(input, min, max){
 
 // Check Password Match
 function checkPasswordsMatch(input1, input2) {
-    if (input1.value !== input2.value){
+    if (input1.value !== input2.value) {
         showError(input2, 'Password do not match');
     }
 }
 
 // Get Field Name
-function getFieldName(input){
+function getFieldName(input) {
     return input.id.charAt(0).toUpperCase() + input.id.slice(1);
 }
 
 
 
 // Event Listeners
-form.addEventListener('submit', function (e){
-e.preventDefault();
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
 
-    checkRequired([username,email,password,password2]);
+    checkRequired([username, email, password, password2]);
     checkLength(username, 3, 15);
     checkLength(password, 6, 25);
     checkEmail(email);
     checkPasswordsMatch(password, password2);
 });
-    
-    
+
+
